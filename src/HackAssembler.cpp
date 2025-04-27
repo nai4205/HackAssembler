cf2b4889c1948c0237a94bca6553f7bbb8c6c5a8
nai4205 <naijonkorn@gmail.com>
2025-04-27 22:39:31 +0100

    removed

--- src/HackAssembler.cpp
+++ src/HackAssembler.cpp
@@ -27,8 +27,6 @@ public:
     }
   }
   void Start() {
-    // TODO: first pass with symbol table
-    //
     first_pass();
     second_pass();
   }
@@ -57,7 +55,6 @@ public:
       }
     }
     file.close();
-    // globalSymbolTable.printTable();
   }
   void second_pass() {
     ifstream file(filename);
@@ -77,27 +74,16 @@ public:
       line.erase(line.find_last_not_of(" \t\n\r\f\v") + 1);
       line.erase(0, line.find_first_not_of(" \t\n\r\f\v"));
       if (!line.empty()) { // Only print non-empty lines
-        // TODO: implement parsing for A commands
         Parser p(line);
         if (line[0] == '@') {
-          // cout << line[0];
           if (std::isdigit(line[1])) {
-            // cout << "number"
-            //       "\n";
             string addr = line.erase(0, 1);
-            // cout << addr;
             bitset<16> binary(stoi(addr));
-            // cout << binary;
             outputFile << binary << "\n";
           } else {
-            // cout << "not number" << "\n";
             string addr = line.erase(0, 1);
-            // cout << "addr: " << addr;
             int val = globalSymbolTable.getValue(addr);
-            // cout << "val: " << val;
             bitset<16> bbinary(val);
-            // cout << "symbol" << addr << "binary" << bbinary << "\n";
-            //  cout << "binary: " << bbinary << "\n";
             outputFile << bbinary << "\n";
           }
         } else if (line[0] != '(') {
@@ -106,16 +92,12 @@ public:
           string c = p.comp;
           string d = p.dest;
           string j = p.jump;
-          // cout << "line" << line << "\n";
           Code cc(c, "comp");
           bitset<7> bc = cc.lookupComp();
-          // cout << "comp" << bc << '\n';
           Code dd(d, "dest");
           bitset<3> bd = dd.lookupDest();
-          // cout << "dest" << bd << '\n';
           Code jj(j, "jump");
           bitset<3> bj = jj.lookupJump();
-          // cout << "jump" << bj << '\n';
           bitset<3> bpad = 0b111;
           outputFile << bpad << bc << bd << bj << "\n";
         }
